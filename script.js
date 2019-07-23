e1dcc0ad7d0f7b14c6f959f1c802fb4916784fb6
ricardo alarcon <geocodinglife@gmail.com>
2019-07-23 05:49:13 -0400

    class methods

---
+++ script.js
@@ -0,0 +1,52 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement  = currentOperandTextElement
+    this.clear()
+  }
+
+  clear() {
+    this.currentOperand  = ''
+    this.previousOperand = ''
+    this.operation       = undefined
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    this.currentOperand = number
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText =  this.currentOperand
+  }
+}
+
+
+
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement  = document.querySelector('[data-current-operand]')
+  const operatinButtons            = document.querySelectorAll('[data-operation]')
+  const numberButtons              = document.querySelectorAll('[data-number]')
+  const allClearButton             = document.querySelector('[data-all-clear')
+  const deleteButton               = document.querySelector('[data-delete]')
+  const equalsButton               = document.querySelector('[data-equals]')
+  const calculator                 = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+    numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+  })
+})
+
